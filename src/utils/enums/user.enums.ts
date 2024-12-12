3e3f0d3f69112d3c30431a991da70b15d9c0da8b
moohaanaad <mohanadahmed266@gmail.com>
2024-12-12 13:10:53 +0200

    add user enum

---
+++ src/utils/enums/user.enums.ts
@@ -0,0 +1,25 @@
+
+
+export enum Gender {
+    MALE = 'male',
+    FEMALE = 'female',
+    OTHER = 'other'
+}
+
+export enum UserRole {
+    USER = 'user',
+    VULONTEER = 'vulonteer',
+    ADMIN = 'admin'
+}
+
+export enum UserStatus {
+    ONLINE = 'online',
+    OFFLINE = 'offline'
+}
+
+export enum ConfirmEmail {
+    PENDING = 'pending',
+    VERIFIED = 'verified',
+    BLOCKED = 'blocked',
+    DELEETED = 'deleted'
+}
