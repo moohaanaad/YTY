700a47464bc7577676136bf9dec0e80d71b184cb
Akuma-b <ashrafelbranc7@gmail.com>
2025-03-26 04:17:38 +0200

    Update admin.guard.ts
    update

--- src/guard/admin.guard.ts
+++ src/guard/admin.guard.ts
@@ -1,14 +1,13 @@
 import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
 import { Request } from "express";
 import { CustomRequest } from "src/types/custom-request.interface";
-import { UserRole } from "src/utils";
+import { UserRole } from "src/utils"; 
 
 @Injectable()
 export class AdminGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<CustomRequest>();
-    const user = request.user;
-    console.log('User:', user); // Debugging
+    const user = request.user; 
 
     if (!user) {
       throw new ForbiddenException("You are not authenticated.");
